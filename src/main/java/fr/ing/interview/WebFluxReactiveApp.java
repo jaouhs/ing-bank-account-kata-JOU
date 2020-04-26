e0be0ec8bb3e11e2338557b0dba327398d521c26
jouhs <jaouad.ouhs@gmail.com>
2020-04-26 03:22:22 +0200

    convert project to spring boot and add mogobd

---
+++ src/main/java/fr/ing/interview/WebFluxReactiveApp.java
@@ -0,0 +1,11 @@
+package fr.ing.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxReactiveApp {
+	public static void main(String[] args) {
+        SpringApplication.run(WebFluxReactiveApp.class, args);
+    }
+}
